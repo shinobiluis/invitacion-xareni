04af84a7e02d7c0ed8aa0c029895181177be3f42
Luis Eduardo Alcantara Olvera <shinobi_luis@hotmail.es>
2022-02-26 18:07:07 -0600

    cmbios

--- js/index.js
+++ js/index.js
@@ -8,4 +8,23 @@ btnPlay.addEventListener('click', iniciar );
 
 function iniciar(){
     musica.play();
+    
+    $( "#btnPlay" ).removeClass( "animate__lightSpeedInRight animate__delay-4s" );
+    $( "#btnPlay" ).addClass( "animate__fadeOutLeft" );
+    $(".wrapperFoco").addClass("animate__animated animate__fadeOutUp animate__delay-2s");
+
+    $( ".wrapperFondo1" ).removeClass( "animate__fadeIn animate__delay-5s" );
+    $( ".wrapperFondo1" ).addClass( "animate__fadeOut animate__delay-2s" );
+
+    
+    $( ".wrapperInvitacion" ).addClass( "animate__animated animate__fadeInUp animate__delay-2s" );
+
+    $(".wrapperInvitacion").css("display", "block");
+    setTimeout(() => {
+        $(".saludo").css("display", "block");    
+    }, 3000);
+    
+
+    
+    
 }
